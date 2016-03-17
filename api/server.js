27e54e2847480112ace358edd8347d14d60a4654
nzarifian <naz.zarifian@gmail.com>
2016-03-17 16:13:26 -0400

    fix button

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ db.once('open', function() {
 });
 
 app.listen(80, function() {
-	console.log('Server Started on http://localhost:80');
+	console.log('Server Started on http://localhost:8080');
 	console.log('Press CTRL + C to stop server');
 });
 
